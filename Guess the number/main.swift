e805eeb9f4184c18ff77abdb0bbec1315d5dd179
angelvz30 <angelvaz3510@gmail.com>
2024-12-18 11:31:51 -0800

    variables for guess game

--- Guess the number/main.swift
+++ Guess the number/main.swift
@@ -13,7 +13,17 @@ print("Guess the number between 1 and 100")
 var randomNumber: Int = Int.random(in: 1...100)
 print(randomNumber)
 
+var guessnil = true
 var guess: Int
 var count: Int = 0
+var notOver = true
 
+
+while notOver{
+    while guessnil{
+        var number = readLine()!
+        var possibleGuess = Int(number)
+        if possibleGuess = nil
+    
+    
 
