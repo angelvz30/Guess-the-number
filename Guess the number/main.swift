4424ee446bcaad61a9ed511d21b18599afe49cb1
angelvz30 <angelvaz3510@gmail.com>
2024-12-20 09:48:37 -0800

    guess number game

--- Guess the number/main.swift
+++ Guess the number/main.swift
@@ -13,17 +13,35 @@ print("Guess the number between 1 and 100")
 var randomNumber: Int = Int.random(in: 1...100)
 print(randomNumber)
 
-var guessnil = true
 var guess: Int
 var count: Int = 0
 var notOver = true
 
 
 while notOver{
-    while guessnil{
-        var number = readLine()!
-        var possibleGuess = Int(number)
-        if possibleGuess = nil
+    var number = readLine()!
+    var possibleGuess = Int(number)
+    if possibleGuess == nil {
+        print("Please enter a number")
+        
+    }
     
-    
-
+    else {
+        guess = possibleGuess!
+        if guess == randomNumber {
+            print("You guessed the correct number!")
+            print("You guessed "+String (count)+" times to answer right!")
+            notOver = false
+            
+        }
+        if guess < randomNumber {
+            print("Your guess is too low guess again")
+            
+            
+        }
+        if guess > randomNumber {
+            print("Your guess is to high guess again")
+            count += 1
+        }
+    }
+}
